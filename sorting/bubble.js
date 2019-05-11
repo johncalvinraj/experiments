7d7e280a0e81a0b0aafc24c849f008bc6c1cb536
kuppi <callyrajkumar@gmail.com>
2019-05-11 15:11:26 +0530

    tab fix

--- sorting/bubble.js
+++ sorting/bubble.js
@@ -3,11 +3,11 @@ for (var i = 0; i <= a.length; i++) {
   var c = 1
   for (var j = 0; j <= a.length-i; j++) {
     var d = a[j]
-	  if (a[j+1] < a[j]){
-	    c = 0
-	    a[j] = a[j+1];
-	    a[j+1] = d;
-	  }
+    if (a[j+1] < a[j]){
+      c = 0
+      a[j] = a[j+1];
+      a[j+1] = d;
+    }
   }
   if(c === 1){
     console.log(a)
